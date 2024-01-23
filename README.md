d347d44a3e82abfdf2ff4cb4d2a55a756a11f83e
vvennela <vishnupratikvennelakanti@gmail.com>
2024-01-22 20:34:52 -0800

    prediction algo

--- README.md
+++
@@ -1,2 +0,0 @@
-# thebooker
-full stack application to predict the results of nba games
--- backend/winner.py
+++ backend/winner.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from model import rf 
+from model import rf, best_rf
 
 df = pd.read_csv("games.csv")
 
@@ -8,28 +8,28 @@ features = ['BEFORE_ELO', 'SLIDING_PTS', 'SLIDING_FGM', 'SLIDING_FGA', 'SLIDING_
             'SLIDING_FT_PCT', 'SLIDING_OREB', 'SLIDING_DREB', 'SLIDING_REB', 'SLIDING_AST',
             'SLIDING_STL', 'SLIDING_BLK', 'SLIDING_TOV', 'SLIDING_PF', 'SLIDING_PLUS_MINUS']
 
-team_name = 'BOS'  # Replace 'YourTeam' with the actual team name
-opp_name = 'MIN'
-# Filter the dataset for the specified team
-team_data1 = df[df['TEAM'] == team_name]
-team_data2 = df[df['TEAM'] == opp_name]
-team_data1 = team_data1[features]
-team_data2 = team_data2[features]
 
-team_data1_array = team_data1[features].values[-1].reshape(1, -1)
-team_data2_array = team_data2[features].values[-1].reshape(1, -1)
+def winner(team_name, opp_name):
+    team_data1 = df[df['TEAM'] == team_name]
+    team_data2 = df[df['TEAM'] == opp_name]
+    team_data1 = team_data1[features]
+    team_data2 = team_data2[features]
 
-# Set feature names for RandomForestClassifier
-rf.feature_names_in_ = features
+    team_data1_array = team_data1[features].values[-1].reshape(1, -1)
+    team_data2_array = team_data2[features].values[-1].reshape(1, -1)
+
+
+    best_rf.feature_names_in_ = features
 
 # Predict probabilities
-probabilities_team1 = rf.predict_proba(team_data1_array)[0][1]  # Probability for class 1 (win)
-probabilities_team2 = rf.predict_proba(team_data2_array)[0][1]  # Probability for class 1 (win)
+    probabilities_team1 = best_rf.predict_proba(team_data1_array)[0][1] 
+    probabilities_team2 = best_rf.predict_proba(team_data2_array)[0][1]  
 
 # Determine the winner based on the higher probability
-winner = team_name if probabilities_team1 > probabilities_team2 else opp_name
+    winner = team_name if probabilities_team1 > probabilities_team2 else opp_name
 
-# Display the results
-print(f"Predicted Probability for {team_name}: {probabilities_team1:.4f}")
-print(f"Predicted Probability for {opp_name}: {probabilities_team2:.4f}")
-print(f"The winner is: {winner}")
+#print(f"Predicted Probability for {team_name}: {probabilities_team1:.4f}")
+#print(f"Predicted Probability for {opp_name}: {probabilities_team2:.4f}")
+    return winner 
+
+winner("BOS", "MIN")
