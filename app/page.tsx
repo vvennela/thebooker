43d53487c652cf618f80359ed0417b47f32a00d5
vvennela <vishnupratikvennelakanti@gmail.com>
2024-01-24 18:23:38 -0800

    completed ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
-import Image from "next/image";
-import React from "react";
-// import styles from '@/app/ui/home.module.css';
+'use client';
+import React, { useState } from "react";
 
 
 export default function Page() {
-  return (
-    <h1 className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-2xl text-white">Hello! Welcome to the Booker!</h1>
-  );
-}
+    const [team1, setTeam1] = useState("");
+    const [team2, setTeam2] = useState("");
 
+    return (
+        <>
+            <div className="flex flex-col items-center justify-center min-h-screen transform translate-y-[-25vh] mb-2">
+                <h1 className="text-4xl text-white">Welcome to theBooker!</h1>
+                <h3 className="text-2xl text-white mt-2">Our prediction algorithm allows you to enter a matchup and see the predicted result</h3>
+                <p>Enter team names/cities or a combination of both (example: ATL, ATLANTA HAWKS, ATLANTA, HAWKS)</p>
+            </div>
+            <div className="flex flex-col items-center justify-center mt-[-35em]">
+                <div className="flex space-x-4">
+                    <input className="rounded-lg p-2 border-2 border-gray-300" name="team1" placeholder="Team 1" value={team1} onChange={(e) => setTeam1(e.target.value)} />
+                    <input className="rounded-lg p-2 border-2 border-gray-300" name="team2" placeholder="Team 2" value={team2} onChange={(e) => setTeam2(e.target.value)} />
+                    <button className="rounded-lg bg-blue-500 text-white p-2">Predict!</button>
+                </div>
+            </div>
+        </>
+    );
+}
 
