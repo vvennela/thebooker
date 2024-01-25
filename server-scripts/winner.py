4cbc0384709b97447a9740bf62d899eae540a13b
vvennela <vishnupratikvennelakanti@gmail.com>
2024-01-24 18:36:52 -0800

    added api endpoint for backend

--- server-scripts/winner.py
+++ server-scripts/winner.py
@@ -1,11 +1,17 @@
+from flask import Flask, request, jsonify
 import pandas as pd
 from model import features, best_xgb
 
-df = pd.read_csv("games.csv")
+app = Flask(__name__)
 
+df = pd.read_csv("games.csv")
 stored_features = features
 
-def winner(team_name, opp_name):
+@app.route('/predict', methods=['GET'])
+def predict():
+    team_name = request.args.get('team')
+    opp_name = request.args.get('opponent')
+
     team_data1 = df[df['TEAM'] == team_name]
     team_data2 = df[df['TEAM'] == opp_name]
     team_data1 = team_data1[stored_features]
@@ -19,7 +25,7 @@ def winner(team_name, opp_name):
 
     winner = team_name if probabilities_team1 > probabilities_team2 else opp_name
 
-    return winner 
+    return jsonify({"winner": winner})
 
-
-print(winner("DET", "CHA"))
+if __name__ == '__main__':
+    app.run(debug=True)
