d5cb9c00e4bdb7f7901b02af7ceb1ee4768f2323
vvennela <vishnupratikvennelakanti@gmail.com>
2024-03-12 18:26:36 -0700

    fixed nfl model

--- backend/nfl/nfl_model.py
+++ backend/nfl/nfl_model.py
@@ -5,7 +5,8 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
 
-df = pd.read_csv("nfl_games.csv", index_col = 0)
+#df = pd.read_csv("nfl_games.csv", index_col = 0)
+df = games_df
 
 features = ['ELO','Sliding_Pts','Sliding_Cmp',
             'Sliding_Att','Sliding_Pass_Yds',
@@ -36,3 +37,31 @@ y_validation = validation_data[label]
 
 X_test = test_data[features]
 y_test = test_data[label]
+
+#param_grid = {
+    #'n_estimators': np.arange(50, , 5),
+    #'max_depth': np.arange(0, 20, 2),
+    #'min_child_weight': np.arange(1, 11, 2),
+    #'gamma': np.arange(0, 5, 0.2)
+#}
+
+param_grid = {
+    "n_estimators": [50, 55, 60],
+    "max_depth": [0, 5, 10],
+    "min_child_weight": [1, 5, 10], 
+    "gamma": [0, 1, 5] 
+}
+
+xgb = XGBClassifier(use_label_encoder=False, eval_metric='logloss')
+grid_search = GridSearchCV(estimator=xgb, param_grid=param_grid, cv=3, scoring='accuracy')
+grid_search.fit(X_train, y_train)
+
+best_xgb = grid_search.best_estimator_
+y_pred_xgb = best_xgb.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred_xgb)
+print(f"Accuracy: {accuracy:.4f}")
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred_xgb))
+print(grid_search.best_params_)
+                                                 
